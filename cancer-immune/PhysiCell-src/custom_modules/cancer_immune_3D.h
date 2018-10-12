51ae035728ec9fdae7bd06814aad40d0d57e80d3
Paul Macklin <Paul.T.Macklin@gmail.com>
2018-10-11 23:59:29 -0400

    update to PhysiCell 1.4.1

--- cancer-immune/PhysiCell-src/custom_modules/cancer_immune_3D.h
+++ cancer-immune/PhysiCell-src/custom_modules/cancer_immune_3D.h
@@ -1,85 +1,75 @@
+/*
+###############################################################################
+# If you use PhysiCell in your project, please cite PhysiCell and the version #
+# number, such as below:                                                      #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1].    #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# See VERSION.txt or call get_PhysiCell_version() to get the current version  #
+#     x.y.z. Call display_citations() to get detailed information on all cite-#
+#     able software used in your PhysiCell application.                       #
+#                                                                             #
+# Because PhysiCell extensively uses BioFVM, we suggest you also cite BioFVM  #
+#     as below:                                                               #
+#                                                                             #
+# We implemented and solved the model using PhysiCell (Version x.y.z) [1],    #
+# with BioFVM [2] to solve the transport equations.                           #
+#                                                                             #
+# [1] A Ghaffarizadeh, R Heiland, SH Friedman, SM Mumenthaler, and P Macklin, #
+#     PhysiCell: an Open Source Physics-Based Cell Simulator for Multicellu-  #
+#     lar Systems, PLoS Comput. Biol. 14(2): e1005991, 2018                   #
+#     DOI: 10.1371/journal.pcbi.1005991                                       #
+#                                                                             #
+# [2] A Ghaffarizadeh, SH Friedman, and P Macklin, BioFVM: an efficient para- #
+#     llelized diffusive transport solver for 3-D biological simulations,     #
+#     Bioinformatics 32(8): 1256-8, 2016. DOI: 10.1093/bioinformatics/btv730  #
+#                                                                             #
+###############################################################################
+#                                                                             #
+# BSD 3-Clause License (see https://opensource.org/licenses/BSD-3-Clause)     #
+#                                                                             #
+# Copyright (c) 2015-2018, Paul Macklin and the PhysiCell Project             #
+# All rights reserved.                                                        #
+#                                                                             #
+# Redistribution and use in source and binary forms, with or without          #
+# modification, are permitted provided that the following conditions are met: #
+#                                                                             #
+# 1. Redistributions of source code must retain the above copyright notice,   #
+# this list of conditions and the following disclaimer.                       #
+#                                                                             #
+# 2. Redistributions in binary form must reproduce the above copyright        #
+# notice, this list of conditions and the following disclaimer in the         #
+# documentation and/or other materials provided with the distribution.        #
+#                                                                             #
+# 3. Neither the name of the copyright holder nor the names of its            #
+# contributors may be used to endorse or promote products derived from this   #
+# software without specific prior written permission.                         #
+#                                                                             #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" #
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE   #
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE  #
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE   #
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR         #
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF        #
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS    #
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN     #
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)     #
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE  #
+# POSSIBILITY OF SUCH DAMAGE.                                                 #
+#                                                                             #
+###############################################################################
+*/
+
 #include "../core/PhysiCell.h"
 #include "../modules/PhysiCell_standard_modules.h" 
 
 using namespace BioFVM; 
 using namespace PhysiCell;
-
-class Cancer_Immune_Options
-{
- private: 
-	
- public:
-	// member data: 
-	
-	// immune cell options 
-	double elastic_coefficient; 
-	double attachment_rate; 
-	double kill_rate; // kill_rate_index
-	double attachment_lifetime; 
-	double immune_cell_apoptosis_rate; 
-	double relative_immune_cell_oxygen_uptake_rate; 
-	double relative_immune_cell_adhesion; 
-	double relative_immune_cell_repulsion; 
-	
-	// cancer cell options
-	double oncoprotein_mean;
-	double oncoprotein_standard_deviation; 
-	double immunostimulatory_factor_secretion_rate_live;
-	double immunostimulatory_factor_secretion_rate_dead; 
-	
-	// overall problem options 
-	double initial_tumor_radius; 
-	double oxygen_conditions; 
-	int number_of_immune_cells; 
-	double domain_size; 
-	
-	// immunostimulatory factor 
-	double immunostimulatory_diffusion_coefficient; 
-	double immunostimulatory_decay_rate; 
-	
-	// for immune_cell_attempt_attachment
-	double oncoprotein_saturation; 
-	double oncoprotein_detection_threshold; // oncoprotein_threshold
-	double max_attachment_distance; 
-	double min_attachment_distance; 
-	
-	double persistence_time; 
-	double migration_speed;
-	double migration_bias; 
-	
-	double break_adhesion_distance; // need to implement -- break the bond if separation is too big
- 
-	// default constructor with default values; 
-	Cancer_Immune_Options();
-	
-/*
-
-	immune_cell.phenotype.motility.is_motile = true; 
-	immune_cell.phenotype.motility.persistence_time = 10.0; 
-	immune_cell.phenotype.motility.migration_speed = 1;  
-	immune_cell.phenotype.motility.migration_bias = 0.5;
-	
-	immune_cell.phenotype.mechanics.cell_cell_adhesion_strength *= 0.0;
-	immune_cell.phenotype.mechanics.cell_cell_repulsion_strength *= 5.0;
-	
-	// set functions 
-	
-	immune_cell.functions.update_phenotype = NULL; 
-	immune_cell.functions.custom_cell_rule = immune_cell_rule; 
-	immune_cell.functions.update_migration_bias = immune_cell_motility;	
-	
-	// set custom data values 
-	
-	immune_cell.custom_data[ "oncoprotein" ] = 0.0; 
-	immune_cell.custom_data[ "kill rate" ] = 1.0/15.0; // how often it tries to kill
-	immune_cell.custom_data[ "attachment lifetime" ] = 60.00; // how long it can stay attached 
-	immune_cell.custom_data[ "attachment rate" ] = 1.0/5.0; // how long it wants to wander before attaching	
-*/	
-	
-	
-};
-
-extern Cancer_Immune_Options cancer_immune_options; 
 
 // custom cell phenotype function to scale immunostimulatory factor with hypoxia 
 void tumor_cell_phenotype_with_and_immune_stimulation( Cell* pCell, Phenotype& phenotype, double dt ); 
@@ -104,7 +94,7 @@ std::vector<std::string> cancer_immune_coloring_function( Cell* );
 // cell rules for extra elastic adhesion
 
 void attach_cells( Cell* pCell_1, Cell* pCell_2 );
-void detach_cells( Cell* pCell_1 , Cell* pCell_2 );
+void dettach_cells( Cell* pCell_1 , Cell* pCell_2 );
 
 void add_elastic_velocity( Cell* pActingOn, Cell* pAttachedTo , double elastic_constant ); 
 void extra_elastic_attachment_mechanics( Cell* pCell, Phenotype& phenotype, double dt );
@@ -120,7 +110,7 @@ void immune_cell_rule( Cell* pCell, Phenotype& phenotype, double dt );
 
 
 void immune_cell_attach( Cell* pAttacker, Cell* pTarget ); // use attach_cells?? 
-void immune_cell_detach( Cell* pAttacker, Cell* pTarget ); // use detach_cells ?? 
+void immune_cell_dettach( Cell* pAttacker, Cell* pTarget ); // use dettach_cells ?? 
 
 
 // immune cell functions for motility 
