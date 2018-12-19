3990eb5051202799f14cb8a07275a79a46b6d0dd
Nick Collier <ncollier@anl.gov>
2018-12-19 09:44:18 -0600

    beagle code changes

--- cancer-immune/EMEWS-scripts/python/get_metrics.py
+++ cancer-immune/EMEWS-scripts/python/get_metrics.py
@@ -18,21 +18,29 @@ def get_tumor_cell_count(instance_dir):
     return tumor_cell_count
 
 def main(root_dir, upf, results_file):
-    import pandas as pd
+    #import pandas as pd
 
-    df = pd.read_csv(upf)
-    df['instance'] = df.index + 1
-    df['tumor_cell_count'] = -2
+    #df = pd.read_csv(upf)
+    #df['instance'] = df.index + 1
+    #df['tumor_cell_count'] = -2
 
     files = glob.glob('{}/instance_*'.format(root_dir))
-    for f in files:
-        instance = int(f[f.rfind("_") + 1 :])
-        count = int(get_tumor_cell_count(f))
-        df.ix[instance - 1, 'tumor_cell_count'] = count
-        #print('{} {}: {}'.format(instance, f, count))
-        #print(df.head())
 
-    df.to_csv(results_file, index=False)
+    with open(results_file, 'w') as f_out:
+        f_out.write('directory,instance,tumor_cell_count\n')
+        for f in files:
+            # only makes sense with upf
+            #instance = int(f[f.rfind("_") + 1 :])
+            idx = f.find("instance_")
+            end = f.rfind("_")
+            instance = f[idx + len('instance_') : end]
+            count = int(get_tumor_cell_count(f))
+            f_out.write("{},{},{}\n".format(os.path.basename(f), instance, count))
+            # df.ix[instance - 1, 'tumor_cell_count'] = count
+            #print('{} {}: {}'.format(instance, f, count))
+            #print(df.head())
+
+    #df.to_csv(results_file, index=False)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
