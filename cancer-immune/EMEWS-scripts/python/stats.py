dd948605b3e370ad070e1ff4a79057b7e98d38c9
Nick Collier <ncollier@anl.gov>
2018-12-19 16:59:34 -0500

    Updated stats to exclude bad value tag

--- cancer-immune/EMEWS-scripts/python/stats.py
+++ cancer-immune/EMEWS-scripts/python/stats.py
@@ -2,17 +2,25 @@ import statistics
 import builtins
 
 def min(vals):
-    fl = [v for x in vals for v in x]
+    fl = [v for x in vals for v in x if v != 9999999999]
+    if len(fl) == 0:
+        return -1
     return builtins.min(fl)
 
 def max(vals):
-    fl = [v for x in vals for v in x]
+    fl = [v for x in vals for v in x if v != 9999999999]
+    if len(fl) == 0:
+        return -1
     return builtins.max(fl)
 
 def mean(vals):
-    fl = [v for x in vals for v in x]
+    fl = [v for x in vals for v in x if v != 9999999999]
+    if len(fl) == 0:
+        return -1
     return statistics.mean(fl)
 
 def std(vals):
-    fl = [v for x in vals for v in x]
+    fl = [v for x in vals for v in x if v != 9999999999]
+    if len(fl) == 0:
+        return -1
     return statistics.pstdev(fl)
